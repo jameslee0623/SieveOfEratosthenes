4a7fa837f7fc5c03019a4056b27eda8f24759538
James Li <James@James-MacBook-Pro.local>
2014-02-08 17:18:49 -0800

    Initial Commit

---
+++ SieveOfEratosthenes/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  SieveOfEratosthenes
+//
+//  Created by James Li on 2/8/14.
+//  Copyright (c) 2014 James Li. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
